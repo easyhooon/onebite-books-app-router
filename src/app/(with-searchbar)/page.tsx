cf372d69ded33e6549ac42979966d54c6a3972ba
easyhooon <jihun.lee@medisolution.co.kr>
2025-03-10 09:47:56 +0900

    [feat] 데이터 캐시 적용
    리퀘스트 메모이제이션과의 차이 확실히 구분

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,11 +1,11 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
-import books from "@/mock/books.json";
 import { BookData } from "@/types";
 
 async function AllBooks() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    { cache: "no-store" }
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다...</div>;
@@ -24,7 +24,8 @@ async function AllBooks() {
 
 async function RecoBooks() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } }
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다...</div>;
